4aadebb4799099f64e6169296dd7801bae6810d9
Deifson Oliveira <nosfied@gmail.com>
2020-01-31 15:36:12 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
 private $tpl;
 private $options = [];
 private $defaults = [
+	"header"=>true,
+	"footer"=>true,
 	"data"=>[]
 ];
 
@@ -30,7 +32,7 @@ public function __construct($opts = array(), $tpl_dir = "/views/"){
 	$this->setData($this->options["data"]);
 
 
-	$this->tpl->draw("header");
+	if ($this->options["header"] === true) $this->tpl->draw("header");
 
 
 }
@@ -57,7 +59,7 @@ public function setTpl($name, $data = array(), $returnHTML = false)
 
 public function __destruct(){
 
-$this->tpl->draw("footer");
+	if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 
 }
